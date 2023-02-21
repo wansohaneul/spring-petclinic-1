dfa587a81026ad2e41a905fb8f3ee877721b1c0b
s4616 <87678241+s4616@users.noreply.github.com>
2023-02-21 21:15:44 +0900

    Update kill_process.sh

--- deploy/scripts/kill_process.sh
+++ deploy/scripts/kill_process.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "Remove existed container"
+sudo chown ubuntu.ubuntu -R /home/ubuntu/scripts
+sudo chown ubuntu.ubuntu -R /home/ubuntu/docker-compose.yml
+
 docker-compose -f /home/ubuntu/docker-compose.yml down || true
